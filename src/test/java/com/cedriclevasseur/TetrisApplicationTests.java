f7024a4800dab1f9f40da4ee01a12fe607e3ad83
Cédric Levasseur <cedric.levasseur@gmail.com>
2016-07-24 00:59:45 +0200

    First version, using http://start.spring.io/

---
+++ src/test/java/com/cedriclevasseur/TetrisApplicationTests.java
@@ -0,0 +1,18 @@
+package com.cedriclevasseur;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = TetrisApplication.class)
+@WebAppConfiguration
+public class TetrisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
