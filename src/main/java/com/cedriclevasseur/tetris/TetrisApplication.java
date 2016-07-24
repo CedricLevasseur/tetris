861ebacf7dfca11b81bb6baa43cfbff80b30a094
Cédric Levasseur <cedric.levasseur@gmail.com>
2016-07-24 22:22:22 +0200

    change pacakage

--- src/main/java/com/cedriclevasseur/tetris/TetrisApplication.java
+++ src/main/java/com/cedriclevasseur/tetris/TetrisApplication.java
@@ -1,4 +1,4 @@
-package com.cedriclevasseur;
+package com.cedriclevasseur.tetris;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
