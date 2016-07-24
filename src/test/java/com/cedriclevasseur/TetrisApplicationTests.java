72bd929880b53afe6f025648a6551533d7adb7ab
Cédric Levasseur <cedric.levasseur@gmail.com>
2016-07-24 22:23:22 +0200

    Added test

--- src/test/java/com/cedriclevasseur/TetrisApplicationTests.java
+++ src/test/java/com/cedriclevasseur/TetrisApplicationTests.java
@@ -1,5 +1,6 @@
 package com.cedriclevasseur;
 
+import com.cedriclevasseur.tetris.TetrisApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
