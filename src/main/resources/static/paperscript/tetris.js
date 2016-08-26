88a7627857f9b619bbd4991c20a327b35cf5f042
Cédric Levasseur <cedric.levasseur@gmail.com>
2016-08-26 13:41:19 +0200

    Adding barspace support for speeding up falling piece

--- src/main/resources/static/paperscript/tetris.js
+++ src/main/resources/static/paperscript/tetris.js
@@ -1,10 +1,11 @@
-var ROTATION_SPEED = 5;
-var FALLING_SPEED=0.25;
+var DEFAUT_ROTATION_SPEED = 5;
+var DEFAUT_FALLING_SPEED=1;
 var angle = 0;
 var rotation = 0;
 var position = 0;
-var linesCompleted = 0;
+var linesCompleted = 0; 
 var piece;
+var fallingSpeed = DEFAUT_FALLING_SPEED;
 
 function drawBlock(x, y, colorIdx) {
     var colorMain = myConfig.listOfColor[colorIdx].colorMain;
@@ -44,17 +45,23 @@ function drawBlock(x, y, colorIdx) {
     return block;
 }
 
-
-function drawPiece(x, y, pieceData) {
+/**
+ * Draw a piece with drawBlock from a schema coming from pieceConfig 
+ * @param {type} x 
+ * @param {type} y
+ * @param {type} pieceConfig
+ * @returns {Group|drawPiece.piece}
+ */
+function drawPiece(x, y, pieceConfig) {
     var xInitial=x;
     var blocPresence = 0;
     var arrayOfBlock = [];
     var block;
     var colorIdx = getRandomInt(0,myConfig.listOfColor.length);
-    console.log(pieceData.name);    
-    for (var lines = 0; lines < pieceData.height; lines++) {
-        for (var rows = 0; rows < pieceData.width; rows++) {
-            blocPresence = pieceData.data[lines][rows];
+    console.log(pieceConfig.name);    
+    for (var lines = 0; lines < pieceConfig.height; lines++) {
+        for (var rows = 0; rows < pieceConfig.width; rows++) {
+            blocPresence = pieceConfig.data[lines][rows];
             if (blocPresence === true) {
                 block = drawBlock(x, y,colorIdx);
                 arrayOfBlock.push(block);
@@ -64,13 +71,13 @@ function drawPiece(x, y, pieceData) {
         x=xInitial;
         y += myConfig.sizeBlock;
     }
-    var pivot = new Path.Circle(new Point(pieceData.pivotX*myConfig.sizeBlock, pieceData.pivotY*myConfig.sizeBlock), 5);
+    var pivot = new Path.Circle(new Point(pieceConfig.pivotX*myConfig.sizeBlock, pieceConfig.pivotY*myConfig.sizeBlock), 5);
     pivot.fillColor = 'black';
     arrayOfBlock.push(pivot);
     var piece = new Group({
         children: arrayOfBlock
     });
-    piece.pivot=new Point(pieceData.pivotX*myConfig.sizeBlock, pieceData.pivotY*myConfig.sizeBlock);
+    piece.pivot=new Point(pieceConfig.pivotX*myConfig.sizeBlock, pieceConfig.pivotY*myConfig.sizeBlock);
     return piece;
 }
 
@@ -84,17 +91,18 @@ var text = new PointText({
 
 var bottomPointY;
 function fallPiece(){
+     console.log("fallPiece");
     if(piece!==undefined){
         bottomPointY = Math.max.apply(Math, [ piece.bounds.bottomLeft.y, piece.bounds.bottomRight.y, piece.bounds.topLeft.y, piece.bounds.topRight.y ]) ;
     }
     if((piece!==undefined)&&( bottomPointY <myConfig.gridHeight)){
-            piece.translate(new Point(0,FALLING_SPEED));        
+            piece.translate(new Point(0,fallingSpeed));        
     }else{
         piece = drawPiece(0, 0, myConfig.listOfPiece[getRandomInt(0,myConfig.listOfPiece.length)]);
     }
     // Create a centered text item at the center of the view:
     //text.content='lines completed : '+linesCompleted+'\nposition:'+(piece!==undefined?piece.position.y:"undefined")+ "\nbottomPosition:"+(piece!==undefined?piece.bottomLeft.y:"undefined");
-    text.content='lines completed : '+linesCompleted+'\nposition:'+(piece!==undefined?piece.position.y:"undefined");
+    text.content='lines completed : '+linesCompleted+'\nfallingSpeed='+fallingSpeed+'\nposition:'+(piece!==undefined?piece.position.y:"undefined");
 }
 
 function move(direction){
@@ -131,15 +139,15 @@ function rotatePiece() {
 function onKeyDown(event) {
         if(event.key==="down"){
             console.log("rotate-left");
-            rotation=-ROTATION_SPEED;
+            rotation=-DEFAUT_ROTATION_SPEED;
         }
         if(event.key==="up"){
             console.log("rotate-right");
-            rotation=ROTATION_SPEED;
+            rotation=DEFAUT_ROTATION_SPEED;
         }
         if(event.key==="space"){
-            console.log("down");
-            console.log("To be implemented");
+            fallingSpeed=DEFAUT_FALLING_SPEED*4;           
+            console.log("speed UP : "+fallingSpeed);
         }
         if(event.key==="left"){
             console.log("move-left");
@@ -148,6 +156,13 @@ function onKeyDown(event) {
         if(event.key==="right"){
             console.log("move-right");
             move("right");
+        }
+}
+function onKeyUp(event) {
+        if(event.key==="space"){
+            
+            fallingSpeed=DEFAUT_FALLING_SPEED;
+            console.log("speed down : "+fallingSpeed);
         }
 }
 
